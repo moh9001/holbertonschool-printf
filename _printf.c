fd49e7b338bf4546c0c14f4c4948f813ae3c68f2
moh9001 <mo9001@gmail.com>
2025-03-26 01:30:35 +0300

    Fixed task 0

--- _printf.c
+++ _printf.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * handle_specifier - Handles format specifier
+ * @format: format string
+ * @args: argument list
+ * @i: current index
+ * Return: number of characters printed
+ */
+int handle_specifier(const char *format, va_list args, int *i)
+{
+	int count = 0;
+
+	switch (format[*i])
+	{
+		case 'c':
+			count += print_char(args);
+			break;
+		case 's':
+			count += print_string(args);
+			break;
+		case '%':
+			count += _putchar('%');
+			break;
+		default:
+			count += _putchar('%');
+			count += _putchar(format[*i]);
+			break;
+	}
+	return (count);
+}
+
+/**
  * _printf - Custom printf function
  * @format: Format string
  * Return: Number of characters printed
@@ -25,23 +55,7 @@ int _printf(const char *format, ...)
 				va_end(args);
 				return (-1);
 			}
-
-			switch (format[i])
-			{
-				case 'c':
-					count += print_char(args);
-					break;
-				case 's':
-					count += print_string(args);
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					count += _putchar('%');
-					count += _putchar(format[i]);
-					break;
-			}
+			count += handle_specifier(format, args, &i);
 		}
 		else
 		{
--- functions.c
+++ functions.c
@@ -8,6 +8,7 @@
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	return (_putchar(c));
 }
 
