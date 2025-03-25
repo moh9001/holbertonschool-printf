f93ff61fc83d5370582355e10416bc5cf80af694
moh9001 <mo9001@gmail.com>
2025-03-26 01:22:27 +0300

    Fix task 0

--- _printf.c
+++ _printf.c
@@ -1,69 +1,55 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * handle_format - Handles format specifiers in _printf
- * @format: The format specifier character
- * @args: The list of arguments
- *
- * Return: Number of characters printed
- */
-int handle_format(char format, va_list args)
-{
-int count = 0;
-
-if (format == 'c') /* Print a single character */
-{
-char c = va_arg(args, int);
-count += write(1, &c, 1);
-}
-else if (format == 's') /* Print a string */
-{
-char *s = va_arg(args, char *);
-if (s)
-count += write(1, s, _strlen(s));
-else
-count += write(1, "(null)", 6);
-}
-else if (format == '%') /* Print a percentage sign */
-{
-count += write(1, "%", 1);
-}
-
-return (count);
-}
-
-/**
- * _printf - Custom implementation of printf function
- * @format: The format string containing text and format specifiers
- *
+ * _printf - Custom printf function
+ * @format: Format string
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	va_list args;
+	int i = 0, count = 0;
 
-if (!format)
-return (-1);
+	if (!format)
+		return (-1);
 
-va_start(args, format);
-while (*format)
-{
-if (*format == '%') /* Handle format specifiers */
-{
-format++;
-if (*format == '\0') /* Handle case where format ends with '%' */
-return (-1);
-count += handle_format(*format, args);
-}
-else /* Print regular characters */
-{
-count += write(1, format, 1);
-}
-format++;
-}
-va_end(args);
-return (count);
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			switch (format[i])
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+					break;
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
--- functions.c
+++ functions.c
@@ -2,24 +2,24 @@
 
 /**
  * print_char - Prints a character
- * @args: va_list args
- * Return: 1
+ * @args: Argument list
+ * Return: Number of characters printed (1)
  */
 int print_char(va_list args)
 {
-char c = va_arg(args, int);
-return (_putchar(c));
+	char c = va_arg(args, int);
+	return (_putchar(c));
 }
 
 /**
  * print_string - Prints a string
- * @args: va_list args
- * Return: number of characters printed
+ * @args: Argument list
+ * Return: Number of characters printed
  */
 int print_string(va_list args)
 {
-	int i = 0;
 	char *str = va_arg(args, char *);
+	int i = 0;
 
 	if (!str)
 		str = "(null)";
