bbd2d8155b0d45adfd81853b84b05473f680e08a
M0simi <mish5508@gmail.com>
2025-03-25 20:33:44 +0000

    done

--- _printf.c
+++ _printf.c
@@ -8,43 +8,45 @@
  *
  * Return: The number of characters printed (excluding the null byte).
  */
+
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+va_list args;
+int count = 0;
 
-	va_start(args, format);
-	while (format && *format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c') /* Handle character format */
-			{
-				char c = va_arg(args, int);
+if (!format) /* تحقق إذا كان format يساوي NULL */
+return (-1);
 
-				count += write(1, &c, 1);
-			}
-			else if (*format == 's') /* Handle string format */
-			{
-				char *s = va_arg(args, char *);
-
-				if (s)
-					count += write(1, s, _strlen(s));
-				else
-					count += write(1, "(null)", 6);
-			}
-			else if (*format == '%') /* Handle '%%' escape */
-			{
-				count += write(1, "%", 1);
-			}
-		}
-		else
-		{
-			count += write(1, format, 1);
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c') /* Handle character format */
+{
+char c = va_arg(args, int);
+count += write(1, &c, 1);
+}
+else if (*format == 's') /* Handle string format */
+{
+char *s = va_arg(args, char *);
+if (s)
+count += write(1, s, _strlen(s));
+else
+count += write(1, "(null)", 6);
+}
+else if (*format == '%') /* Handle '%%' escape */
+{
+count += write(1, "%", 1);
+}
+}
+else
+{
+count += write(1, format, 1);
+}
+format++;
+}
+va_end(args);
+return (count);
 }
